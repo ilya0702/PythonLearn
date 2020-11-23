19abee2850466c4665f69533b776985d7f0e6198
ilya <x-noop@mail.ru>
2020-11-23 15:39:43 +0600

    task 4.1 done

--- exercises/04_data_structures/task_4_1.py
+++ exercises/04_data_structures/task_4_1.py
@@ -1,12 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Задание 4.1
-
-Используя подготовленную строку nat, получить новую строку, в которой
-в имени интерфейса вместо FastEthernet написано GigabitEthernet.
-
-Ограничение: Все задания надо выполнять используя только пройденные темы.
-
-"""
-
 nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
+natnew = nat.replace('Fast', 'Gigabit')
+print(natnew)
+
