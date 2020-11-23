772a642d37612ba17c34d0de45f974ac6552cfd2
ilya <x-noop@mail.ru>
2020-11-23 21:50:01 +0600

    task 5.2 done

--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -24,3 +24,27 @@ Out[1]: '11111111111111111111111111110000'
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip = input('Enter Ip Address:')
+ip = ip.replace('/', '.')
+list1 = ip.split('.')
+print('Network:')
+print('{:10}{:10}{:10}{:10}'.format(*list1))
+print('{:08b}  {:08b}  {:08b}  {:08b}'.format(int(list1[0]), int(list1[1]), int(list1[2]), int(list1[3])))
+print('\n')
+print('Mask:')
+print('/' + list1[-1])
+mask = int(list1[-1])
+ones = mask * '1'
+zeros = (32 - mask) * '0'
+fullmask = ones + zeros
+oct1 = fullmask[0:8]
+oct2 = fullmask[8:16]
+oct3 = fullmask[16:24]
+oct4 = fullmask[24:32]
+print('{:<10}{:<10}{:<10}{:<10}'.format(int(oct1, 2), int(oct2, 2), int(oct3, 2), int(oct4, 2)))
+print('{:<10}{:<10}{:<10}{:<10}'.format(oct1, oct2, oct3, oct4))
+
+
+
+
