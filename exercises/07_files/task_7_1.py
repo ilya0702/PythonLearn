980b0a0dbc8c72f9c5b72614e33b5df30f8be44c
ilya0702 <x-noop@mail.ru>
2020-11-25 00:24:04 -0500

    task 7.1 done

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -13,3 +13,14 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+with open('ospf.txt', 'r') as file:
+	for line in file:
+		line = line.replace('via', ',').replace('O', '').replace(',', '').strip().split()
+		print('{:20}{:20}'.format('Prefix', line[0]))
+		print('{:20}{:20}'.format('AD/Metric', line[1]))
+		print('{:20}{:20}'.format('Next-Hop', line[2]))
+		print('{:20}{:20}'.format('Last update', line[3]))
+		print('{:20}{:20}'.format('Outbound Interface', line[4]))
+		print()
+		
