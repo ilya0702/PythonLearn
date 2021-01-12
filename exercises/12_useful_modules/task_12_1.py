cc7e3bc67d93e2dd31e6667011ae90d0d35cb2ed
ilya0702 <x-noop@mail.ru>
2021-01-12 11:13:21 +0600

    task 12.1 done

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -14,3 +14,24 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+import subprocess
+
+
+def ping_ip_addresses(ip_address):
+	avlist = []
+	navlist = []
+	restuple = ()
+	for ip in ip_address:
+		ping = subprocess.run(['ping', '-c', '3', '-n', ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
+		if ping.returncode == 0:
+			avlist.append(ip)
+		else:
+			navlist.append(ip)
+	restuple = (avlist, navlist)
+	return restuple
+
+if __name__ == "__main__":
+	iplist = ['8.8.8.8', 'a', '8.8.4.4', 'n.n.n', 'g.g']
+	print(ping_ip_addresses(iplist))
+
